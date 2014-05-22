910042453fd3d41c7695b17ff5ec98cad10322dc
Sam Yi <sam@codemettle.com>
2014-05-22 15:40:04 -0400

    "roll duel"

--- src/main/scala/Dice.scala
+++ src/main/scala/Dice.scala
@@ -21,6 +21,41 @@ class Dice {
       params.getOrElse(key, "(unknown " + key + ")")
     }
 
+    def fight(a: String, b: String): String = {
+      attack(a, b) + attack(b, a) + checkLife(a) + checkLife(b) + checkWin()
+    }
+
+    def attack(attacker: String, defender: String): String = {
+      val hp: Int = players.get(defender).get
+      val dmg: Int = roll(10)
+      val newHp: Int = hp - dmg
+
+      players = players + ((defender, newHp))
+
+      attacker + " hit " + defender + " for " + dmg + ". "
+    }
+
+    def checkLife(player: String): String = {
+      val hp: Int = players.get(player).get
+      if (hp <= 0) {
+        players = players - player
+        player + " is daed! :( "
+      } else {
+        player + " has " + hp + " HP. "
+      }
+    }
+
+    def checkWin(): String = {
+      if (players.size == 1) {
+        val grats:String = players.keysIterator.next() + " is the winner!"
+        players = players.empty
+        grats
+      } else {
+        ""
+      }
+    }
+
+
     if (get("text").startsWith("%21start+")) {
       val playerList: List[String] = get("text").stripPrefix("%21start+").split("\\+").toList
       players = playerList.map(name => (Web.decode(name), 50)).toMap
@@ -28,19 +63,11 @@ class Dice {
       None
     } else if (players.keySet.contains(get("user_name"))) {
       if (players.keySet.contains(get("text"))) {
-        val victim: String = get("text")
-        val hp: Int = players.get(victim).get
-        val dmg: Int = roll(10)
-        val newHp: Int = hp - dmg
-        val result: String =
-          if (newHp <= 0) {
-            players = players - victim
-            victim + " is daed! :("
-          } else {
-            victim + " has " + newHp + " HP."
-          }
-        val resp: String = get("user_name") + " hit " + victim + " for " + dmg + ". " + result
-        post(resp)
+        val attacker: String = get("user_name")
+        val defender: String = get("text")
+        val result: String = fight(attacker, defender)
+
+        post(result)
         None
       } else {
         val directMessage: String = get("text") + " isn't in the game!"
