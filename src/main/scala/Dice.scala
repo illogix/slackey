f4ca8c1b39f8e690ff309289c513a2d87a160a2d
Sam Yi <sam@codemettle.com>
2014-05-23 11:23:38 -0400

    remove tokens

--- src/main/scala/Dice.scala
+++ src/main/scala/Dice.scala
@@ -7,82 +7,82 @@ import scalaj.http.Http
  */
 class Dice {
 
-  var players: Map[String, Int] = Map[String, Int]()
+    var players: Map[String, Int] = Map[String, Int]()
 
-  val dice = new Random
+    val dice = new Random
 
-  def roll(sides: Int): Int = dice.nextInt(sides) + 1
+    def roll(sides: Int): Int = dice.nextInt(sides) + 1
 
-  def Dice() = {
-  }
-
-  def process(params: Map[String, String]): Option[String] = {
-    def get(key: String): String = {
-      params.getOrElse(key, "(unknown " + key + ")")
+    def Dice() = {
     }
 
-    def fight(a: String, b: String): String = {
-      attack(a, b) + checkLife(a) + ", " + checkLife(b) + checkWin()
+    def process(params: Map[String, String]): Option[String] = {
+        def get(key: String): String = {
+            params.getOrElse(key, "(unknown " + key + ")")
+        }
+
+        def fight(a: String, b: String): String = {
+            attack(a, b) + checkLife(a) + ", " + checkLife(b) + checkWin()
+        }
+
+        def attack(a: String, b: String): String = {
+            hit(a)
+            hit(b)
+            a + ":sword:" + b + "! "
+        }
+
+        def hit(player: String) = {
+            val hp: Int = players.get(player).get
+            val dmg: Int = roll(10)
+            val newHp: Int = hp - dmg
+            players = players + ((player, newHp))
+        }
+
+        def checkLife(player: String): String = {
+            val hp: Int = players.get(player).get
+            if (hp <= 0) {
+                players = players - player
+                player + " is dead! :skull:"
+            } else {
+                player + ": " + hp + "HP"
+            }
+        }
+
+        def checkWin(): String = {
+            if (players.size == 1) {
+                val grats: String = ".  " + players.keysIterator.next() + " wins! :trophy::thumbsup:"
+                players = players.empty
+                grats
+            } else {
+                ""
+            }
+        }
+
+        if (get("text").startsWith("%21start+")) {
+            val playerList: List[String] = get("text").stripPrefix("%21start+").split("\\+").toList
+            players = playerList.map(name => (Web.decode(name), 50)).toMap
+            post("Starting game with 50 HP for the following players: " + playerList.mkString(", "))
+            None
+        } else if (players.keySet.contains(get("user_name"))) {
+            if (players.keySet.contains(get("text"))) {
+                val attacker: String = get("user_name")
+                val defender: String = get("text")
+                val result: String = fight(attacker, defender)
+
+                post(result)
+                None
+            } else {
+                val directMessage: String = get("text") + " isn't in the game!"
+                Some(directMessage)
+            }
+        } else {
+            val directMessage: String = "You aren't in the game!"
+            Some(directMessage)
+        }
     }
 
-    def attack(a: String, b: String): String = {
-      hit(a)
-      hit(b)
-      a + ":sword:" + b + "! "
+    def post(text: String) = {
+        val postParams: List[(String, String)] = List(("username", "rollbot"), ("icon_emoji", ":game_die:"), ("text", text))
+        Http.post(Web.inWebHookURL).params("payload" -> Web.makeJson(postParams)).asString
     }
-
-    def hit(player: String) = {
-      val hp: Int = players.get(player).get
-      val dmg: Int = roll(10)
-      val newHp: Int = hp - dmg
-      players = players + ((player, newHp))
-    }
-
-    def checkLife(player: String): String = {
-      val hp: Int = players.get(player).get
-      if (hp <= 0) {
-        players = players - player
-        player + ": :skull:"
-      } else {
-        player + ": " + hp + "HP"
-      }
-    }
-
-    def checkWin(): String = {
-      if (players.size == 1) {
-        val grats:String = "  " + players.keysIterator.next() + ":trophy::thumbsup:"
-        players = players.empty
-        grats
-      } else {
-        ""
-      }
-    }
-
-    if (get("text").startsWith("%21start+")) {
-      val playerList: List[String] = get("text").stripPrefix("%21start+").split("\\+").toList
-      players = playerList.map(name => (Web.decode(name), 50)).toMap
-      post("Starting game with 50 HP for the following players: " + playerList.mkString(", "))
-      None
-    } else if (players.keySet.contains(get("user_name"))) {
-      if (players.keySet.contains(get("text"))) {
-        val attacker: String = get("user_name")
-        val defender: String = get("text")
-        val result: String = fight(attacker, defender)
-
-        post(result)
-        None
-      } else {
-        val directMessage: String = get("text") + " isn't in the game!"
-        Some(directMessage)
-      }
-    } else {
-      val directMessage: String = "You aren't in the game!"
-      Some(directMessage)
-    }
-  }
-
-  def post(text: String) = {
-    val postParams: List[(String, String)] = List(("username", "rollbot"), ("icon_emoji", ":game_die:"), ("text", text))
-    Http.post(Web.inWebHookURL).params("payload" -> Web.makeJson(postParams)).asString
-  }
 }
--- src/main/scala/Web.scala
+++ src/main/scala/Web.scala
@@ -11,91 +11,88 @@ import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse}
 import util.Properties
 
 object Web {
-  // Tokens
-  val outWebHookToken: String = "REwjJhtU7rbm6fyAXt7FCni6"
-  val slashRollToken: String = "b731yQBiwC3cwg24IlGjiEXS"
-  val inTestingToken: String = "rFCX9chyUckjizCRPeuAwryX"
+    // THESE SHOULD BE SET SOMEWHERE AS CONFIG VARS
+    val outWebHookToken: String = System.getenv("OUT_WEBHOOK_TOKEN")
+    val slashRollToken: String = System.getenv("SLASH_ROLL_TOKEN")
+    val inWebHookURL: String = System.getenv("IN_WEBHOOK_URL")
 
-  // URLs
-  val inWebHookURL: String = "https://poundc.slack.com/services/hooks/incoming-webhook?token=" + inTestingToken
-
-  def decode(urlEncoded: String): String = {
-    URLDecoder.decode(urlEncoded, "UTF-8")
-  }
-
-  def makeJson(kvs: List[(String, String)]): String = {
-    def makeInside(kv: (String, String)): String = {
-      "\"" + kv._1 + "\": \"" + kv._2.replace("\\", "\\\\").replace("\"", "\\\"") + "\""
+    def decode(urlEncoded: String): String = {
+        URLDecoder.decode(urlEncoded, "UTF-8")
     }
-    val inside = kvs map ((kv: (String, String)) => makeInside(kv)) mkString ", "
-    "{" + inside + "}"
-  }
 
-  def main(args: Array[String]) {
-    val port = Properties.envOrElse("PORT", "8080").toInt
-    println("Starting on port:" + port)
-    ServerBuilder()
-      .codec(Http())
-      .name("slackey-server")
-      .bindTo(new InetSocketAddress(port))
-      .build(new Slackey)
-    println("Started.")
-  }
+    def makeJson(kvs: List[(String, String)]): String = {
+        def makeInside(kv: (String, String)): String = {
+            "\"" + kv._1 + "\": \"" + kv._2.replace("\\", "\\\\").replace("\"", "\\\"") + "\""
+        }
+        val inside = kvs map ((kv: (String, String)) => makeInside(kv)) mkString ", "
+        "{" + inside + "}"
+    }
+
+    def main(args: Array[String]) {
+        val port = Properties.envOrElse("PORT", "8080").toInt
+        println("Starting on port:" + port)
+        ServerBuilder()
+            .codec(Http())
+            .name("slackey-server")
+            .bindTo(new InetSocketAddress(port))
+            .build(new Slackey)
+        println("Started.")
+    }
 }
 
 class Slackey extends Service[HttpRequest, HttpResponse] {
 
-  // webHook POST example
-  //  token=REwjJhtU7rbm6fyAXt7FCni6&team_id=T02A3F3HL&team_domain=poundc&service_id=2343925644&channel_id=C02A3F3HW&channel_name=optimization&timestamp=1400726065.001431&user_id=U02A2NEUX&user_name=illo&text=%21sup%5Cteh%22son
+    // webHook POST example
+    //  token=abcdefghABCDEFGH12345678&team_id=T0001&team_domain=mydomain&service_id=1234567890&channel_id=C2147483705&channel_name=optimization&timestamp=1400726065.001431&user_id=U2147483697&user_name=bob&text=%21hello
 
-  // slash POST example
-  //  token=b731yQBiwC3cwg24IlGjiEXS&team_id=T0001&channel_id=C2147483705&channel_name=test&user_id=U2147483697&user_name=illo&command=/roll&text=maki
+    // slash POST example
+    //  token=abcdefghABCDEFGH12345678&team_id=T0001&channel_id=C2147483705&channel_name=test&user_id=U2147483697&user_name=alice&command=/roll&text=maki
 
-  val dice: Dice = new Dice
+    val dice: Dice = new Dice
 
-  def apply(req: HttpRequest): Future[HttpResponse] = {
-    val postParams: List[String] = req.getContent.toString(Charset.forName("UTF-8")).split("&").toList
-    val params = postParams.map(pp => {
-      pp.split("=") match {
-        case Array(x, y) => (x, y)
-        case Array(x) => (x, "")
-        case _ => ("", "")
-      }
-    }).toMap
-    process(params)
-  }
-
-  def process(params: Map[String, String]): Future[HttpResponse] = {
-    def get(key: String): String = {
-      params.getOrElse(key, "(unknown " + key + ")")
+    def apply(req: HttpRequest): Future[HttpResponse] = {
+        val postParams: List[String] = req.getContent.toString(Charset.forName("UTF-8")).split("&").toList
+        val params = postParams.map(pp => {
+            pp.split("=") match {
+                case Array(x, y) => (x, y)
+                case Array(x) => (x, "")
+                case _ => ("", "")
+            }
+        }).toMap
+        process(params)
     }
 
-    if (get("token") == Web.outWebHookToken) {
-      if (get("text").startsWith("%21")) {
-        // trigger: starts with !
-        val resp: String = "Hi " + get("user_name") + ", you said: " + Web.decode(get("text")).substring(1)
-        respond(Some(resp))
-      } else if (get("text").startsWith("%24")) {
-        // trigger: starts with $
-        val resp: String = get("user_name") + " talkin bout " + Web.decode(get("text")).substring(1) + " dollars"
-        respond(Some(resp))
-      } else {
-        respond(None)
-      }
-    } else if (get("token") == Web.slashRollToken) {
-      respond(dice.process(params), json = false)
-    } else {
-      respond(None)
-    }
-  }
+    def process(params: Map[String, String]): Future[HttpResponse] = {
+        def get(key: String): String = {
+            params.getOrElse(key, "(unknown " + key + ")")
+        }
 
-  def respond(resp: Option[String], json: Boolean = true): Future[HttpResponse] = {
-    val response = Response()
-    response.setStatusCode(200)
-    resp match {
-      case Some(respText) => response.setContentString(if (json) Web.makeJson(List(("text", respText))) else respText)
-      case None =>
+        if (get("token") == Web.outWebHookToken) {
+            if (get("text").startsWith("%21")) {
+                // trigger: starts with !
+                val resp: String = "Hi " + get("user_name") + ", you said: " + Web.decode(get("text")).substring(1)
+                respond(Some(resp))
+            } else if (get("text").startsWith("%24")) {
+                // trigger: starts with $
+                val resp: String = get("user_name") + " talkin bout " + Web.decode(get("text")).substring(1) + " dollars"
+                respond(Some(resp))
+            } else {
+                respond(None)
+            }
+        } else if (get("token") == Web.slashRollToken) {
+            respond(dice.process(params), json = false)
+        } else {
+            respond(None)
+        }
     }
-    Future(response)
-  }
+
+    def respond(resp: Option[String], json: Boolean = true): Future[HttpResponse] = {
+        val response = Response()
+        response.setStatusCode(200)
+        resp match {
+            case Some(respText) => response.setContentString(if (json) Web.makeJson(List(("text", respText))) else respText)
+            case None =>
+        }
+        Future(response)
+    }
 }
