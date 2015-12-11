8b1773f352bb4c17c8a9407040cba10c2c7460ac
Sam Yi <sam@codemettle.com>
2015-12-10 23:30:39 -0500

    more blackjack

--- src/main/scala/org/samyi/slackey/bots/casbot/Casbot.scala
+++ src/main/scala/org/samyi/slackey/bots/casbot/Casbot.scala
@@ -1,6 +1,7 @@
 package org.samyi.slackey.bots.casbot
 
 import akka.actor.{ActorRef, Actor, Props}
+import akka.pattern.ask
 import org.samyi.slackey.Req
 
 /**
@@ -52,11 +53,107 @@ class Player(name: String, var balance: Long) extends Actor {
   }
 }
 
+case class Card(value: Int, suit: Int) {
+  def getValueString: String = value match {
+    case 1 => "*A*"
+    case 11 => "*J*"
+    case 12 => "*Q*"
+    case 13 => "*K*"
+    case v => s"*${v.toString}*"
+  }
 
-object Blackjack {
-  def props = Props(classOf[Blackjack])
+  def getSuitString: String = suit match {
+    case 1 => ":hearts:"
+    case 2 => ":clubs:"
+    case 3 => ":diamonds:"
+    case 4 => ":spades:"
+    case _ => ":skull:"
+  }
 }
 
-class Blackjack {
+object Deck {
+  def props = Props(classOf[Deck])
+  case object GetCard
+  case class NextCard(card: Card)
+}
 
+class Deck extends Actor {
+  import Deck._
+  var usedCards: Set[Card] = Set.empty
+  def receive = {
+    case GetCard => ???
+  }
+}
+
+// 1P for now
+object Blackjack {
+  def props(player: ActorRef) = Props(classOf[Blackjack], player)
+  case class Ante(amt: Long)
+  case object Hit
+  case object Stand
+  case object Fold
+  trait Result
+  case object Win extends Result
+  case object Lose extends Result
+  case class Status(dealerCards: List[Card], playerCards: List[Card], result: Option[Result])
+}
+
+class Blackjack(player: ActorRef) extends Actor {
+  import Blackjack._
+  var dealerCards: List[Card] = List.empty
+  var playerCards: List[Card] = List.empty
+
+  val deck = context.system.actorOf(Deck.props)
+
+  def deal() = {
+
+  }
+
+  def getCardValue(card: Card, high: Boolean): Int = card.value match {
+    case 1 => if (high) 11 else 1
+    case v if v < 11 => v
+    case _ => 10
+  }
+
+  def getTotal(cards: List[Card]) = {
+    cards.foldLeft(0)((sum, card) => sum + getCardValue(card, high = true))
+  }
+
+  def getResult = {
+    ???
+  }
+
+  def getStatus = Status(dealerCards, playerCards, getResult)
+
+  def receive = {
+    case Ante(amt) =>
+      // check UpdateBalance here?
+      context become dealing
+  }
+
+  def dealing: Receive = {
+    case Deck.NextCard(card) if dealerCards.length == 2 && playerCards.length == 2 =>
+      // check
+      context become waitingPlayer
+    case Deck.NextCard(card) =>
+
+  }
+
+  def waitingPlayer: Receive = {
+    case Hit =>
+      deck ! Deck.GetCard
+      context.become(waitingPlayerCard)
+    case Stand =>
+    case Fold =>
+  }
+
+  def waitingPlayerCard: Receive = {
+    case Deck.NextCard(card) =>
+      playerCards = playerCards :+ card
+      context.unbecome()
+  }
+
+  def dealersTurn: Receive = {
+    case Deck.NextCard(card) =>
+  }
 }
